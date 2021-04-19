6b318eab351aa0f7604235b3a6dddb1219435fd3
Tan Shuwen <tsw822@gmail.com>
2021-04-19 19:09:54 -0400

    implement qrcode for pwa order

---
+++ PopupMealEssentials.js
@@ -0,0 +1,168 @@
+const Order = require("./Order");
+
+const OrderState = Object.freeze({
+    WELCOMING:   Symbol("welcoming"),
+    FOOD:   Symbol("food"),
+    TOFU:   Symbol("tofu"),
+    SOUP:  Symbol("soup"),
+    PAYMENT: Symbol("payment")
+});
+
+module.exports = class PopupMealEssentials extends Order{
+    constructor(sNumber, sUrl){
+        super(sNumber, sUrl);
+        this.stateCur = OrderState.WELCOMING;
+        this.sType = "";
+        this.sFood = "";
+        this.sSpicyTofu = "";
+        this.sSoup = "";
+        this.nTotal = 0;
+    }
+    handleInput(sInput){
+        let aReturn = [];
+        switch(this.stateCur){
+            case OrderState.WELCOMING:
+                this.stateCur = OrderState.FOOD;
+                aReturn.push("Welcome to POPUP MEALS Waterloo.");
+                aReturn.push(`For the menu of popup meals please tap:`);
+                aReturn.push(`${this.sUrl}/menu/${this.sNumber}/`);
+                if(sInput.toLowerCase() == "hot"){
+                  this.sType = "spicy";
+                }else if(sInput.toLowerCase() == "yum") {
+                  this.sType = "normal";
+                } else {
+                  this.stateCur = OrderState.WELCOMING;
+                  aReturn.push("Please type HOT if you want spicy meal or YUM if you want non-spicy meal.");
+                  break;
+                }
+                aReturn.push("What Main course would you like CHICKEN, OTHERS, or NO?");
+                break;
+            case OrderState.FOOD:
+                if(sInput.toLowerCase()!= "no"){
+                  this.sFood = sInput;
+                }
+                if(this.sType == "spicy"){
+                    this.stateCur = OrderState.TOFU;
+                    aReturn.push("Would you like spicy Tofo?");
+                }else{
+                    this.stateCur = OrderState.SOUP;
+                    aReturn.push("Would you like meat ball SOUP for your meal?");
+                }
+                break;
+            case OrderState.TOFU:
+                this.stateCur = OrderState.SOUP
+                if(sInput.toLowerCase()!= "no"){
+                  this.sSpicyTofu = "spicy Tofo";
+                }
+                aReturn.push("Would you like meat ball SOUP for your meal?");
+                break;
+            case OrderState.SOUP:
+                if(sInput.toLowerCase() != "no"){
+                    this.sSoup = sInput;
+                }
+                aReturn.push("Thank-you for your order of");
+
+                if(this.sType == "spicy" && this.sFood.toLowerCase() == "chicken"){
+                  aReturn.push("spicy chicken meal");
+                  this.nTotal += 13.99;
+                }else if(this.sType == "spicy" && this.sFood.toLowerCase() == "others"){
+                  aReturn.push("delecious spicy meal");
+                  this.nTotal += 16.99
+                }else if(this.sType == "normal" && this.sFood.toLowerCase() == "chicken"){
+                  aReturn.push("delecious chicken meal");
+                  this.nTotal += 13.99;
+                }else if(this.sType == "normal" && this.sFood.toLowerCase() == "others"){
+                  aReturn.push("delecious non-spicy meal");
+                  this.nTotal += 16.99
+                }
+                if(this.sSpicyTofu){
+                  aReturn.push(this.sSpicyTofu);
+                  this.nTotal += 6.99;
+                }
+                if(this.sSoup){
+                  aReturn.push(this.sSoup);
+                  this.nTotal += 5.99;
+                }
+
+                aReturn.push(`Your total comes to ${this.nTotal}`);
+                if(this.nTotal >0){
+                    this.stateCur = OrderState.PAYMENT;
+                    this.nOrder = this.nTotal;
+                    aReturn.push(`Please pay for your order here`);
+                    aReturn.push(`${this.sUrl}/payment/${this.sNumber}/`);
+                    }
+                else{
+                    aReturn.push("Thank-you, but your order beyond our service.Welcome your new order.");
+                    this.isDone(true);
+                    this.stateCur = OrderState.WELCOMING;
+                }
+                break;
+            case OrderState.PAYMENT:
+                var obj = JSON.parse(JSON.stringify(sInput));
+                var target = obj.purchase_units[0].shipping;
+                var name = target.name.full_name;
+                var address = target.address.address_line_1 +" "+
+                              target.address.admin_area_2 + " "+
+                              target.address.postal_code;
+                this.isDone(true);
+                let d = new Date(); 
+                d.setMinutes(d.getMinutes() + 20);
+                aReturn.push(`Please pick it up at ${d.toTimeString()} for ${name} to ${address}`);
+                aReturn.push(`We will text you from 519-222-2222 when your order is ready or if we have questions.`)
+                break;
+        }
+        return aReturn;
+    }
+    renderForm(){
+      // your client id should be kept private
+            const sClientID = process.env.SB_CLIENT_ID || 'put your client id here for testing ... Make sure that you delete it before committing'
+
+      return(`
+      <!DOCTYPE html>
+  
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+      </head>
+      
+      <body>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script
+          src="https://www.paypal.com/sdk/js?client-id=${sClientID}"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+        </script>
+        Thank you ${this.sNumber} for your order of $${this.nOrder}.
+        <div id="paypal-button-container"></div>
+  
+        <script>
+          paypal.Buttons({
+              createOrder: function(data, actions) {
+                // This function sets up the details of the transaction, including the amount and line item details.
+                return actions.order.create({
+                  purchase_units: [{
+                    amount: {
+                      value: '${this.nOrder}'
+                    }
+                  }]
+                });
+              },
+              onApprove: function(data, actions) {
+                // This function captures the funds from the transaction.
+                return actions.order.capture().then(function(details) {
+                  // This function shows a transaction success message to your buyer.
+                  $.post(".", details, ()=>{
+                    window.open("", "_self");
+                    window.close(); 
+                  });
+                });
+              }
+          
+            }).render('#paypal-button-container');
+          // This function displays Smart Payment Buttons on your web page.
+        </script>
+      
+      </body>
+          
+      `);
+  
+    }
+}
--- index.js
+++ index.js
@@ -11,9 +11,14 @@ server.listen(port, function () {
   console.log("Server listening at port %d", port);
 });
 
-const LockDownEssentials = require("./LockDownEssentials");
+const PopupMealEssentials = require("./PopupMealEssentials");
 const e = require('express');
 const { exception } = require('console');
+
+// var id="12345A";
+// var name = "Test";
+// var address = "test address";
+var paymentDetailReq = null;
 
 // Create a new express application instance
 
@@ -22,6 +27,71 @@ app.use(express.static("www"));
 
 let oSockets = {};
 let oOrders = {};
+let resScope;
+
+app.post("/payment/", (req, res) => {
+  var total = req.body.total;
+  var orderId = req.body.orderId;
+  var phone = req.body.phone;
+
+  console.log(total,orderId,phone);
+  function renderForm(){
+    // your client id should be kept private
+    const sClientID = process.env.SB_CLIENT_ID || 'put your client id here for testing ... Make sure that you delete it before committing'
+    return(`
+    <!DOCTYPE html>
+
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+    </head>
+    
+    <body>
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+      <script
+      src="https://www.paypal.com/sdk/js?client-id=${sClientID}"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+      </script>
+      Thank you ${phone} for your order of $${total}.
+      <div id="paypal-button-container"></div>
+
+      <script>
+        paypal.Buttons({
+            createOrder: function(data, actions) {
+              // This function sets up the details of the transaction, including the amount and line item details.
+              return actions.order.create({
+                purchase_units: [{
+                  amount: {
+                    value: '${total}'
+                  }
+                }]
+              });
+            },
+            onApprove: function(data, actions) {
+              // This function captures the funds from the transaction.
+              return actions.order.capture().then(function(details) {
+                // This function shows a transaction success message to your buyer.
+                $.post(".", details, ()=>{
+                  window.open("", "_self");
+                  window.close(); 
+                });
+              });
+            }
+        
+          }).render('#paypal-button-container');
+        // This function displays Smart Payment Buttons on your web page.
+      </script>
+
+      <p>After your payment, please click the QR Code link below:</p>
+      <a href="http://localhost:3002/QRCode">QR Code</a>
+    
+    </body>
+        
+    `);
+    
+  }
+  res.end(renderForm());
+});
+
 app.post("/payment/:phone", (req, res) => {
   // this happens when the order is complete
   sFrom = req.params.phone;
@@ -55,12 +125,18 @@ app.get("/payment/:phone", (req, res) => {
   }
 });
 
+app.get("/menu/:phone", (req, res) => {
+  
+  // this happens when the user clicks on the link in SMS
+  res.redirect("http://127.0.0.1:5500/menu.html");
+});
+
 app.post("/sms", (req, res) => {
   // turn taking SMS
   let sFrom = req.body.From || req.body.from;
   let sUrl = `${req.headers['x-forwarded-proto'] || req.protocol}://${req.headers['x-forwarded-host'] || req.headers.host}${req.baseUrl}`;
   if (!oOrders.hasOwnProperty(sFrom)) {
-    oOrders[sFrom] = new LockDownEssentials(sFrom, sUrl);
+    oOrders[sFrom] = new PopupMealEssentials(sFrom, sUrl);
   }
   if (oOrders[sFrom].isDone()) {
     delete oOrders[sFrom];
@@ -75,6 +151,74 @@ app.post("/sms", (req, res) => {
     sResponse += "</Message>";
   }
   res.end(sResponse + "</Response>");
+});
+
+app.post("/",(req,res)=>{
+  var obj = JSON.parse(JSON.stringify(req.body));
+  paymentDetailReq = obj;
+  
+  // console.log(paymentDetailReq);
+});
+
+app.get("/QRCode/",(req,res)=>{
+
+  if (!!paymentDetailReq){
+    var obj = paymentDetailReq
+    var id = obj.id;
+    var target = obj.purchase_units[0].shipping;
+    var name = target.name.full_name;
+    var address = target.address.address_line_1 +" "+
+                  target.address.admin_area_2 + " "+
+                  target.address.postal_code;
+    function renderQRCode(){
+      return(`
+      <!DOCTYPE html>
+      <html lang="en">
+      
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title class="title">Payment</title>
+          <script type="text/javascript" src="qrcode.js"></script>
+      </head>
+      <body>
+          <h1 class="title">You have completed your payment</h1>
+          <p>Your order will be sent to</p>
+          <p name="buyerName" id="buyerName"> ${name}</p>
+          <p name="address" id="address">${address}</p>
+          <p>Order QR Code is below, please keep that for receiving your order</p>
+          <div id="placeHolder"></div>
+          <script>
+              var typeNumber = 5;
+              var errorCorrectionLevel = 'L';
+              var qr = qrcode(typeNumber, errorCorrectionLevel);
+              qr.addData('${id}');
+              qr.make();
+              document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+          </script>
+      </body>
+      `);  
+    }
+    paymentDetailReq=null;
+    res.end(renderQRCode());
+}
+  else{
+    res.end("Please pay first");
+  }
+
+});
+
+
+app.get("/QRCode/:phone",(req,res)=>{
+  const sFrom = req.params.phone;
+  // if (!oOrders.hasOwnProperty(sFrom)) {
+  //   res.end("order already complete");
+  // } else {
+  //   res.end(oOrders[sFrom].renderForm());
+  // }
+    // paymentDetailReq=null;
+    res.end(oOrders[sFrom].renderQRCode());
 });
 
 io.on('connection', function (socket) {
